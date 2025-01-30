6368751f8b3ebe1123762895b3546bcb96d7637f
punnammanikanta <PUNNAMMANIKANTA52@GMAIL.COM>
2025-01-30 11:54:08 +0530

    Add files via upload

---
+++ Asmd.java
@@ -0,0 +1,15 @@
+public class Asmd {
+   
+        public static void main(String[]args){
+        int a = 27;
+        int b = 35;
+        System.out.println(a-b);
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    
+    }
+    }
+    
+    
+
